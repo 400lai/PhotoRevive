608b3e4fe145bc75812fd33cc49769479d4dad29
laiiiii <3176213938@qq.com>
2025-11-23 19:08:57 +0800

    项目创建+启动页SplashActivity完成

---
+++ app/src/main/java/com/laiiiii/photorevive/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.laiiiii.photorevive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottom_navigation)
+
+        // 默认显示“修图”页面
+        loadFragment(EditFragment())
+
+        bottomNav.setOnItemSelectedListener { item ->
+            val selectedFragment: Fragment = when (item.itemId) {
+                R.id.nav_edit -> EditFragment()
+                R.id.nav_inspiration -> InspirationFragment()
+                R.id.nav_mine -> MineFragment()
+                else -> EditFragment()
+            }
+            loadFragment(selectedFragment)
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/laiiiii/photorevive/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.laiiiii.photorevive
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 模拟启动延迟
+        // Handler显示传入 Looper
+        Handler(Looper.getMainLooper()).postDelayed({
+            // 启动主页面 MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            // 关闭启动页面
+            finish()
+        }, 2000)    // 2秒延迟
+    }
+
+}
